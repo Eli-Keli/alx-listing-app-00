69b1a6834cdfcf5f70115786f12443c3ce0c2cd8
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-21 13:06:53 +0300

    Add PropertyProps interface and sample property listings data

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,19 @@
 // Define all TypeScript interfaces related to the project. Start by creating placeholder interfaces for CardProps and ButtonProps.
+export interface PropertyProps {
+    name: string;
+    address: {
+        state: string;
+        city: string;
+        country: string;
+    };
+    rating: number;
+    category: string[];
+    price: number;
+    offers: {
+        bed: string;
+        shower: string;
+        occupants: string;
+    };
+    image: string;
+    discount: string;
+}
